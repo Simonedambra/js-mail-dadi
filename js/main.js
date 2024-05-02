4114c2b5addb47948c22a2c9b2ab7783584e0ae8
Simonedambra <simonedambra130102@gmail.com>
2024-05-02 17:49:58 +0200

    [upd]

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 'use strict'
 
-const mail = ['simone@gmail.com', 'giuseppe@gmail.com', 'daniele@gmail.com', 'francesco@gmail.com', 'edoardo@gmail.com', 'roberto@gmail.com', 'angelo@gmail.com', 'gianni@gmail.com', 'davide@gmail.com', 'lorenzo@gmail.com', 'mauro@gmail.com', 'marco@gmail.com',]
+let mail = ['simone@gmail.com', 'giuseppe@gmail.com', 'daniele@gmail.com', 'francesco@gmail.com', 'edoardo@gmail.com', 'roberto@gmail.com', 'angelo@gmail.com', 'gianni@gmail.com', 'davide@gmail.com', 'lorenzo@gmail.com', 'mauro@gmail.com', 'marco@gmail.com',]
 
-console.log(mail)
+let emailSelection = prompt('inserisci la tua email').toLowerCase();
 
-let emailSelection = prompt('inserisci la tua email')
-emailSelection = ('')
+// variabile flag
+let isEmailValid = false;
+// loop for
+for (let i = 0; i < mail.length; i++) {
+    //console.log(mail[i]);
 
-
-if (emailSelection === emailSelection) {
-    console.log('hai inserito correttamente la tua email')
+    if (mail[i] === emailSelection) {
+        isEmailValid = true;
+    }
 }
-else if (emailSelection === '') {
-    console.log('non hai inserito alcuna email!')
+
+
+//check
+if (isEmailValid === true) {
+    console.log('hai inserito correttamente la tua email')
 }
 else {
     console.log('hai inserito una email non presente nella lista')
 }
+
+
+
+
+
+
